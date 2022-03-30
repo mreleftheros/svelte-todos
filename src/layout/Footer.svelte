71010090e509547ea97ab2626b306eb8f0c55e56
eleftheros <eleftheros_all@protonmail.com>
2022-03-30 19:31:56 +0300

    Add footer

---
+++ src/layout/Footer.svelte
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<footer class="footer">
+  <p class="footer-text">
+    Copyright &copy; 2022 
+    <span class="footer-title">made by mreleftheros</span>
+  </p>
+</footer>
+
+<style>
+  .footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    grid-area: f;
+    background: #333;
+    padding: .3rem;
+  }
+
+  .footer-text {
+    font-size: 1.4rem;
+  }
+
+  .footer-title {
+    font-style: italic;
+    letter-spacing: .1rem;
+  }
+</style>
---
+++ src/layout/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<header class="header">
+  hello header
+</header>
+
+<style>
+  .header {
+    grid-area: h;
+  }
+</style>
